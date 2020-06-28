f4687c8f10a8d16b6237798d07141bd01254adec
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-06-28 18:35:40 +0300

    create blog.urls and add path

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    title = 'I am Kek'
+    context = {
+        'title': title,
+    }
+    return render(request, 'pages/home.html', context)
